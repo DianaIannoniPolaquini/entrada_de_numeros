8723d1001aa1c2eae340d1b6e61885b7b8720edd
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-04 23:28:56 -0300

    primeiro commit

---
+++ ENTRADA_DE_CÓDIGO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ENTRADA_DE_CÓDIGO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+
+            Console.WriteLine("Entre com um número inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+
+            while (n != 0)
+            {
+                soma += n;
+                n = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Soma de todos os número: {0}.", 
+                soma);
+        }
+    }
+}
